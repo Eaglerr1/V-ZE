9c1dc695e672eb46d0bbddb094168a5d874ca318
Eaglerr1 <akkusertugrulgazi10@gmail.com>
2025-01-14 18:03:12 +0300

    ÖDEV
    React.js VİZE ÖDEVİ

---
+++ myfirst-react-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>React projesinin ilk uygulaması.</h1>
+    </>
+  );
+}
+
+export default App;
